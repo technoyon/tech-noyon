961af54757c28f2f3142c7333b70dfc432d00e9e
technoyon <ntechsite2026@gmail.com>
2026-02-19 00:23:14 -0600

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -20,7 +20,6 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'http://localhost:4000'
-
-// export const url = 'https://ntech-backend.onrender.com'
+export const url = 'https://tech-noyon3.onrender.com'
+// export const url = 'http://localhost:4000'
 
